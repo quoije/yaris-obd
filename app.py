5e8e3cf603c01cf7006564ad2ac8b60b827f2ed1
quoije <mathieumoran1@gmail.com>
2024-03-09 15:37:12 -0500

    put prod server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, make_response, json
 import app_obd, app_car, app_account, time
 from app_events import socketio
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 socketio.init_app(app)
@@ -25,3 +26,12 @@ def data():
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
+
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    print("server on :5000")
+    http_server.serve_forever()
+    
